5884fe306344a2dd5a50a70a861c75cc1486cb61
isinov <disinov@syntes.io>
2021-10-04 11:49:40 +0300

    algorhytm tasks

--- musicGenres/genres_in.sql
+++ musicGenres/genres_in.sql
@@ -1,3 +1,26 @@
+CREATE TABLE genre
+(
+    id bigint PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    parent_genre_id bigint,
+    FOREIGN KEY (parent_genre_id) REFERENCES genre(id)
+);
+
+CREATE TABLE track
+(
+    id bigint PRIMARY KEY ,
+    name varchar(100) NOT NULL
+);
+
+CREATE TABLE track_genre
+(
+    track_id bigint,
+    genre_id bigint,
+    PRIMARY KEY(track_id, genre_id),
+    FOREIGN KEY (track_id) REFERENCES track(id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
+);
+
 INSERT INTO genre(id, name, parent_genre_id)
 VALUES
     (1, 'pop', null),
